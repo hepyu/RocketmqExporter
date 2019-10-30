2a5be7a7987a23f3488bfe78759d1c9e3be737ae
hpy <hpy253215039@163.com>
2019-10-30 16:52:08 +0800

    update

--- RocketmqExporter.go
+++ RocketmqExporter.go
@@ -3,16 +3,43 @@ package main
 import (
 	"RocketmqExporter/constant"
 	"fmt"
+	//"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"net/http"
+	"time"
 )
 
 func main() {
 
+	promlogConfig := &promlog.Config{}
+	fmt.Println(promlogConfig)
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	fmt.Println(promlogConfig)
+	kingpin.Version(version.Print("rocketmq_exporter"))
+	kingpin.Parse()
+
+	logger := promlog.New(promlogConfig)
+
+	level.Info(logger).Log("msg", "Starting rocketmq_exporter", "version", version.Info)
+	level.Info(logger).Log("msg", "Build contenxt", version.BuildContext())
+
 	metricsPath := constant.GetMetricsPath()
 	listenAddress := constant.GetListenAddress()
 	metricsPrefix := constant.GetMetricsPrefix()
+
+	fmt.Println("fmt.metricsPath:" + metricsPath)
+	fmt.Println("fmt.listenAddress:" + listenAddress)
+	fmt.Println("fmt.metricsPrefix:" + metricsPrefix)
+
+	level.Info(logger).Log("msg", "fmt.metricsPath:"+metricsPath)
+
+	time.Sleep(3 * time.Hour)
 
 	exporter := DeclareExporter(metricsPrefix)
 	prometheus.MustRegister(exporter)
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module RocketmqExporter
 
 go 1.13
 
-require github.com/prometheus/client_golang v1.2.1
+require (
+	github.com/go-kit/kit v0.9.0
+	github.com/prometheus/client_golang v1.2.1
+	github.com/prometheus/common v0.7.0
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
